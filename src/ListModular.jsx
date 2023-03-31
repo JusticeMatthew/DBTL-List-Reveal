fc1087f605b57cf4c3892e857e41df43a659c22d
unknown <ls41513@yahoo.com>
2023-03-31 02:05:59 -0400

    Move style/setStyle function into button onClick

--- src/ListModular.jsx
+++ src/ListModular.jsx
@@ -21,135 +21,25 @@ export function ListModular() {
 
     const [active, setActive] = useState("");
 
-    const changeStyleJ1 = () => {
-        if (styleJ1 == "hide") {
-            setStyleJ1("unhide");
-            if (matchImage != J1[1]) { setMatchImage(J1[1]) };
-            setActive(J1[2])
-        }
-        else {
-            setStyleJ1("hide");
-            if (matchImage == J1[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
-    const changeStyleJ2 = () => {
-        if (styleJ2 == "hide") {
-            setStyleJ2("unhide");
-            if (matchImage != J2[1]) { setMatchImage(J2[1]) };
-            setActive(J2[2])
-        }
-        else {
-            setStyleJ2("hide");
-            if (matchImage == J2[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
-    const changeStyleJ3 = () => {
-        if (styleJ3 == "hide") {
-            setStyleJ3("unhide");
-            if (matchImage != J3[1]) { setMatchImage(J3[1]) };
-            setActive(J3[2])
-        }
-        else {
-            setStyleJ3("hide");
-            if (matchImage == J3[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
-    const changeStyleJ4 = () => {
-        if (styleJ4 == "hide") {
-            setStyleJ4("unhide");
-            if (matchImage != J4[1]) { setMatchImage(J4[1]) };
-            setActive(J4[2])
-        }
-        else {
-            setStyleJ4("hide");
-            if (matchImage == J4[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
-    const changeStyleJ5 = () => {
-        if (styleJ5 == "hide") {
-            setStyleJ5("unhide");
-            if (matchImage != J5[1]) { setMatchImage(J5[1]) };
-            setActive(J5[2])
-        }
-        else {
-            setStyleJ5("hide");
-            if (matchImage == J5[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
-    const changeStyleD1 = () => {
-        if (styleD1 == "hide") {
-            setStyleD1("unhide");
-            if (matchImage != D1[1]) { setMatchImage(D1[1]) };
-            setActive(D1[2])
-        }
-        else {
-            setStyleD1("hide");
-            if (matchImage == D1[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
-    const changeStyleD2 = () => {
-        if (styleD2 == "hide") {
-            setStyleD2("unhide");
-            if (matchImage != D2[1]) { setMatchImage(D2[1]) };
-            setActive(D2[2])
-        }
-        else {
-            setStyleD2("hide");
-            if (matchImage == D2[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
-    const changeStyleD3 = () => {
-        if (styleD3 == "hide") {
-            setStyleD3("unhide");
-            if (matchImage != D3[1]) { setMatchImage(D3[1]) };
-            setActive(D3[2])
-        }
-        else {
-            setStyleD3("hide");
-            if (matchImage == D3[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
-    const changeStyleD4 = () => {
-        if (styleD4 == "hide") {
-            setStyleD4("unhide");
-            if (matchImage != D4[1]) { setMatchImage(D4[1]) };
-            setActive(D4[2])
-        }
-        else {
-            setStyleD4("hide");
-            if (matchImage == D4[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
-    const changeStyleD5 = () => {
-        if (styleD5 == "hide") {
-            setStyleD5("unhide");
-            if (matchImage != D5[1]) { setMatchImage(D5[1]) };
-            setActive(D5[2])
-        }
-        else {
-            setStyleD5("hide");
-            if (matchImage == D5[1]) { setMatchImage(null); setActive("") }
-        }
-    }
-
     const resetImage = () => { setMatchImage(); setActive("") }
 
     return (
         <div className="list-modular">
             <div className="single-list">
                 <div className="list-name">John's List:</div>
-                <button
+                <button 
                     className={"list-item " + styleJ1}
-                    onClick={changeStyleJ1}
+                    onClick={() => {
+                        if (styleJ1 == "hide") {
+                            setStyleJ1("unhide");
+                            if (matchImage != J1[1]) { setMatchImage(J1[1]) };
+                            setActive(J1[2])
+                        }
+                        else {
+                            setStyleJ1("hide");
+                            if (matchImage == J1[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(J1[2]).valueOf())
                         && (styleJ1 == "unhide")
                         ? "active-left"
@@ -159,7 +49,17 @@ export function ListModular() {
                 </button>
                 <button
                     className={"list-item " + styleJ2}
-                    onClick={changeStyleJ2}
+                    onClick={() => {
+                        if (styleJ2 == "hide") {
+                            setStyleJ2("unhide");
+                            if (matchImage != J2[1]) { setMatchImage(J2[1]) };
+                            setActive(J2[2])
+                        }
+                        else {
+                            setStyleJ2("hide");
+                            if (matchImage == J2[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(J2[2]).valueOf())
                         && (styleJ2 == "unhide")
                         ? "active-left"
@@ -169,7 +69,17 @@ export function ListModular() {
                 </button>
                 <button
                     className={"list-item " + styleJ3}
-                    onClick={changeStyleJ3}
+                    onClick={() => {
+                        if (styleJ3 == "hide") {
+                            setStyleJ3("unhide");
+                            if (matchImage != J3[1]) { setMatchImage(J3[1]) };
+                            setActive(J3[2])
+                        }
+                        else {
+                            setStyleJ3("hide");
+                            if (matchImage == J3[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(J3[2]).valueOf())
                         && (styleJ3 == "unhide")
                         ? "active-left"
@@ -179,7 +89,17 @@ export function ListModular() {
                 </button>
                 <button
                     className={"list-item " + styleJ4 }
-                    onClick={changeStyleJ4}
+                    onClick={() => {
+                        if (styleJ4 == "hide") {
+                            setStyleJ4("unhide");
+                            if (matchImage != J4[1]) { setMatchImage(J4[1]) };
+                            setActive(J4[2])
+                        }
+                        else {
+                            setStyleJ4("hide");
+                            if (matchImage == J4[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(J4[2]).valueOf())
                         && (styleJ4 == "unhide")
                         ? "active-left"
@@ -189,7 +109,17 @@ export function ListModular() {
                 </button>
                 <button
                     className={"list-item " + styleJ5}
-                    onClick={changeStyleJ5}
+                    onClick={() => {
+                        if (styleJ5 == "hide") {
+                            setStyleJ5("unhide");
+                            if (matchImage != J5[1]) { setMatchImage(J5[1]) };
+                            setActive(J5[2])
+                        }
+                        else {
+                            setStyleJ5("hide");
+                            if (matchImage == J5[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(J5[2]).valueOf())
                         && (styleJ5 == "unhide")
                         ? "active-left"
@@ -211,7 +141,17 @@ export function ListModular() {
                 <div className="list-name">Dom's List:</div>
                 <button
                     className={"list-item " + styleD1 }
-                    onClick={changeStyleD1}
+                    onClick={() => {
+                        if (styleD1 == "hide") {
+                            setStyleD1("unhide");
+                            if (matchImage != D1[1]) { setMatchImage(D1[1]) };
+                            setActive(D1[2])
+                        }
+                        else {
+                            setStyleD1("hide");
+                            if (matchImage == D1[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(D1[2]).valueOf())
                         && (styleD1 == "unhide")
                         ? "active-right"
@@ -221,7 +161,17 @@ export function ListModular() {
                 </button>
                 <button
                     className={"list-item " + styleD2}
-                    onClick={changeStyleD2}
+                    onClick={() => {
+                        if (styleD2 == "hide") {
+                            setStyleD2("unhide");
+                            if (matchImage != D2[1]) { setMatchImage(D2[1]) };
+                            setActive(D2[2])
+                        }
+                        else {
+                            setStyleD2("hide");
+                            if (matchImage == D2[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(D2[2]).valueOf())
                         && (styleD2 == "unhide")
                         ? "active-right"
@@ -231,7 +181,17 @@ export function ListModular() {
                 </button>
                 <button
                     className={"list-item " + styleD3}
-                    onClick={changeStyleD3}
+                    onClick={() => {
+                        if (styleD3 == "hide") {
+                            setStyleD3("unhide");
+                            if (matchImage != D3[1]) { setMatchImage(D3[1]) };
+                            setActive(D3[2])
+                        }
+                        else {
+                            setStyleD3("hide");
+                            if (matchImage == D3[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(D3[2]).valueOf())
                         && (styleD3 == "unhide")
                         ? "active-right"
@@ -241,7 +201,17 @@ export function ListModular() {
                 </button>
                 <button
                     className={"list-item " + styleD4}
-                    onClick={changeStyleD4}
+                    onClick={() => {
+                        if (styleD4 == "hide") {
+                            setStyleD4("unhide");
+                            if (matchImage != D4[1]) { setMatchImage(D4[1]) };
+                            setActive(D4[2])
+                        }
+                        else {
+                            setStyleD4("hide");
+                            if (matchImage == D4[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(D4[2]).valueOf())
                         && (styleD4 == "unhide")
                         ? "active-right"
@@ -251,7 +221,17 @@ export function ListModular() {
                 </button>
                 <button
                     className={"list-item " + styleD5}
-                    onClick={changeStyleD5}
+                    onClick={() => {
+                        if (styleD5 == "hide") {
+                            setStyleD5("unhide");
+                            if (matchImage != D5[1]) { setMatchImage(D5[1]) };
+                            setActive(D5[2])
+                        }
+                        else {
+                            setStyleD5("hide");
+                            if (matchImage == D5[1]) { setMatchImage(null); setActive("") }
+                        }
+                    }}
                     id={ (String(active).valueOf() == String(D5[2]).valueOf())
                         && (styleD5 == "unhide")
                         ? "active-right"
